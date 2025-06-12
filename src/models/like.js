f7d04b0469e687fd50b8684e72d2ff3ab976f5b3
smit_mewada <smit.mewada@instinctinnovations.com>
2025-06-12 21:51:02 +0530

    Added like model and basic user model

---
+++ src/models/like.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+  onModel:{
+    type: String,
+    required: true,
+    enum: ['Tweet', 'Comment']
+  },
+  likeable: {
+    type: mongoose.Schema.Typses.ObjectId, 
+    required: true,
+    refPath: 'onModel'  // dynamic reference 
+  },
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {timestamps: true});
+
+const Like = mongoose.model('Like', likeSchema);
+
+module.exports = Like;
